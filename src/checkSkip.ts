aceaa0acca04fc68f59f9fa02234d27e00672b15
lmssee <lmssee@outlook.com>
2024-06-02 16:30:50 +0800

    version: 0.0.22   - 更新了依赖  - 优化了应用在 windows 上的性能

---
+++ src/checkSkip.ts
@@ -0,0 +1,12 @@
+import hotData from "./hotData";
+/** 检验文件是否为跳过的文件，避免多次触发
+ *
+ * @param filename   监听者子
+ */
+export default function (filename: string): boolean {
+  // 过滤掉 .git 及 node_modules 文件
+  return (
+    /(^\.)|(\.git)|(node_modules)/.test(filename) ||
+    hotData.options.skip.test(filename)
+  );
+}
